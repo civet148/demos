11be29ee041df1f08a3ea505d637130c3b72bdb5
libin <civet148@126.com>
2020-11-09 17:17:36 +0800

    [demos/algrithoms] reverse integer

--- algrithoms/reverse-int/main.go
+++ algrithoms/reverse-int/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 /*
 算法描述：翻转数值（假设给出的数值大于0，例如 123 翻转后321）
@@ -9,14 +12,24 @@ import "fmt"
 */
 
 func main() {
-	var num = 12345
+	var num int32 = 2147483647 //前提条件：正整数
 	fmt.Printf("reverse %d = %d \n", num, rotateNumber(num))
 }
 
-func rotateNumber(number int) (n int) {
+func rotateNumber(x int32) int32 {
 
-	for i := 0; number > 0; i++ {
-
+	var y int64 //先用int64存储，否则无法跟MaxInt32/MinInt32进行比较
+	for {
+		if x == 0 {
+			break
+		}
+		y = y*10 + int64(x%10)
+		x = x / 10
 	}
-	return
+
+	fmt.Printf("y = int64(%d) \n", y)
+	if y > math.MaxInt32 || y < math.MinInt32 { // -2147483648 ~ 2147483647
+		return 0
+	}
+	return int32(y)
 }
